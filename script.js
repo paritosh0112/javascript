657a549228030c581d9948ecf9279607b25eeee9
paritosh <paritoshpurohit29@gmail.com>
2025-02-27 10:07:52 +0530

    new files

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-console.log("hello");
-let arr =[1,2,3,4,5];
-console.log(arr);
+//arrays
+// let marks=[95,56,77,88,65]
+// console.log(marks);
+// console.log(marks.length);//property gives an value
+// let heroes=["ironman","ranveer","spiderman","thor","hulk"];
+// console.log(heroes.length);
+// console.log(typeof (heroes));
+// console.log(marks[0]);
+// marks[3]=90;
+// console.log(marks);
+//LOOPING AN ARRRAY
+// let heroes=["ironman","ranveer","spiderman","thor","hulk","kapoor"];
+// for (let idx=0; idx<heroes.length; idx++){
+//     console.log(heroes[idx]);
+// }
+// let cities =["mumbai","haryana","delhi","banswara","dungarpur"];
+// for(city of cities){
+//     console.log(city);
+// }
+
+
+
