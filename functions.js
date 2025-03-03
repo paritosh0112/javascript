07e4aac3554e607fe2e16d04b0e87f4997cb53c6
paritosh <paritoshpurohit29@gmail.com>
2025-03-03 11:06:52 +0530

    functionss

---
+++ functions.js
@@ -0,0 +1,37 @@
+// function myFunction(){
+//     console.log("welcome to web development");
+//     console.log("we are learning js");
+// }
+// myFunction() ;
+
+// function myFunction(msg){   // input parameter 
+//          console.log(msg);
+// }
+// myFunction("i love javascript");  //argument
+
+//FUNCTION for sum of 2 numbers 
+// function sum(x,y){
+//LOCAL VARIABLES
+//     console.log(x+y);
+// }
+// sum (100,500);
+//return 
+// function sum(x,y){
+//     s=x+y;
+//     return s;
+// }
+// let val=sum (100,7);
+// console.log(val);
+//function expression
+// let sum=function(a,b){
+//     return a+b;
+// }
+// console.log(sum);
+// sum(1,2);
+// const arrowsum=(a,b)=>{ //function variable 
+//     console.log(a+b);
+// }
+// arrowsum(3,6);
+()=>{
+    console.log("hello");
+}
