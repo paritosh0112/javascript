3083aeecabf855719d145172732f0ac3c627409c
paritosh <paritoshpurohit29@gmail.com>
2025-03-03 11:27:51 +0530

    arrow que

--- functiionspracticeques.js
+++ functiionspracticeques.js
@@ -1,16 +1,32 @@
-function countVowels(str){
+// function countVowels(str){
+//     let count=0;
+//       for (const char of str){
+//         if
+//         (char=="a" ||
+//          char=="e" ||
+//          char=="i" ||
+//          char=="o" ||
+//          char=="u"
+//         ){
+//             count++;
+//         }
+//       }
+//       console.log(count);
+// }
+// countVowels("paritosh");
+//by arrow function
+ const countVowels=(str)=>{
     let count=0;
-      for (const char of str){
-        if
-        (char=="a" ||
-         char=="e" ||
-         char=="i" ||
-         char=="o" ||
-         char=="u"
-        ){
-            count++;
-        }
+    for (const char of str){
+      if
+      (char=="a" ||
+       char=="e" ||
+       char=="i" ||
+       char=="o" ||
+       char=="u"
+      ){
+          count++;
       }
-      console.log(count);
+    }
+    console.log(count);
 }
-countVowels("paritosh");
