5f35429376e21bd55733151a5324045ba4e80406
paritosh <paritoshpurohit29@gmail.com>
2025-03-24 09:21:26 +0530

    ForeachPractice

---
+++ practicequeForEach.js
@@ -0,0 +1,10 @@
+// let array =[55,66,77,34];
+// array.forEach((val)=>{
+//      console.log(val*val);
+// })
+//dusra method likhna hai
+let array =[55,66,77,34];
+let calsqaure=(val) => {
+    console.log(val*val);
+}
+array.forEach(calsqaure);
