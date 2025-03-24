3c904f007e7ad029f6279c84805052db0e9fd187
paritosh <paritoshpurohit29@gmail.com>
2025-03-24 10:50:39 +0530

    filterque

--- practicequesARRAyMethod.js
+++ practicequesARRAyMethod.js
@@ -1,4 +1,4 @@
-// we are given array marks of students. filter out the marks students above 90 +.
+//Q 1 we are given array marks of students. filter out the marks students above 90 +.
 let marks =[55,44,97,92,91,77];
 let output =marks.filter((val)=>{
         return val >90;
