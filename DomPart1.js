17afa4449041a05a32d4047513c8e421edfb4ea9
paritosh <paritoshpurohit29@gmail.com>
2025-03-25 09:46:54 +0530

    dom

--- DomPart1.js
+++ DomPart1.js
@@ -1,2 +1,18 @@
-let heading = document.getElementById("heading");
-console.log(heading);
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+//  let headings=document.getElementsByClassName("heading");
+//  console.log(headings);
+// let paras=document.getElementsByTagName("p");
+// console.log(paras)
+// let firstelements=document.querySelector("p");  //for 1st elemnt
+// console.dir(firstelements)
+// let allelements=document.querySelectorAll("p");  // for all elements
+// console.dir(allelements)
+// let firstelements=document.querySelector(".heading");  //for 1st elemnt
+// console.dir(firstelements)
+// let allelements=document.querySelectorAll(".heading");  // for all elements
+// console.dir(allelements)
+// let firstelements=document.querySelector("p");  //for 1st elemnt
+// console.dir(firstelements)
+let div=document.querySelector("div");
+console.log(div); 
