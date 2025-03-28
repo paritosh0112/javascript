9d1247e77b871c5817467e325b08859704afaabc
paritosh <paritoshpurohit29@gmail.com>
2025-03-28 09:33:17 +0530

    DomPart2

---
+++ DOMPART2.js
@@ -0,0 +1,12 @@
+// let div=document.querySelector("div");
+// console.log(div);
+
+// let id=div.getAttribute("id")
+// console.log(id)
+
+// let para=document.querySelector("p");
+// console.log(para.setAttribute("class","newclass"));
+
+// let div=document.querySelector("div");
+// div.style.backgroundColor="green"
+// div.style.fontsize="50px";
