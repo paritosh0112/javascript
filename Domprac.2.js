f95df99a4e146aa345e10e19c50fbefd5010c620
paritosh <paritoshpurohit29@gmail.com>
2025-04-03 10:07:44 +0530

    Dom last question compeltee

--- Domprac.2.js
+++ Domprac.2.js
@@ -1,6 +1,10 @@
-let newBtn=document.createElement("button");
-newBtn.innerText="click me";
-newBtn.style.color="white";
-newBtn.style.backgroundColor="green";
-let body=document.querySelector("body");
-body.prepand(newBtn);
+//Q 1
+// let newBtn=document.createElement("button");
+// newBtn.innerText="click me";
+// newBtn.style.color="white";
+// newBtn.style.backgroundColor="green";
+// let body=document.querySelector("body");
+// body.prepand(newBtn);
+
+//Q 2
+let para=document.querySelector("p");
