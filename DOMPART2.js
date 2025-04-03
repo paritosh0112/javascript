f61dbe634aa4030f26de9ad4248302e6b140c468
paritosh <paritoshpurohit29@gmail.com>
2025-04-03 09:42:35 +0530

    Dom complete

--- DOMPART2.js
+++ DOMPART2.js
@@ -7,6 +7,28 @@
 // let para=document.querySelector("p");
 // console.log(para.setAttribute("class","newclass"));
 
+//NODE STYLE
 // let div=document.querySelector("div");
 // div.style.backgroundColor="green"
-// div.style.fontsize="50px";
+// div.style.fontsize="100px";
+// div.style.visibility="hidden";
+// div.innerText="paritosh";
+
+//add new element 
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me !";
+// console.log(newbtn);
+
+// let div=document.querySelector("div");
+// div.append(newbtn);  //at end 
+// div.prepend(newbtn); //at first matlab upper 
+// div.before(newbtn);   // matlab div ke bhaar
+// div.after(newbtn);
+
+// let newHeading=document.createElement("h1");
+// newHeading.innerText="never give mere bhai";
+// let body=document.querySelector("body");
+// body.prepend(newHeading);
+
+
+
