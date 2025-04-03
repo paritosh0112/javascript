ff015bdac82dad49c686079155767c60531121ba
paritosh <paritoshpurohit29@gmail.com>
2025-04-03 10:07:26 +0530

    Dom last question compeltee

---
+++ Domprac.2.js
@@ -0,0 +1,6 @@
+let newBtn=document.createElement("button");
+newBtn.innerText="click me";
+newBtn.style.color="white";
+newBtn.style.backgroundColor="green";
+let body=document.querySelector("body");
+body.prepand(newBtn);
